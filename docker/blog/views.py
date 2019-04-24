927d6fa46edd14243609d5b86ded744dd29fc906
dizzyplay <vovoboss@gmail.com>
2019-04-24 15:51:22 +0900

    add

--- docker/blog/views.py
+++ docker/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def main(request):
+    qs = Post.objects.all()
+
+    return render(request, 'blog/main.html',{
+        'qs':qs
+    })
